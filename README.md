1b7e7b3bfdea20ba7916e3a3fe5b42bd2f969e71
seotory <seotory@seotoryui-MacBook-Air.local>
2017-01-27 21:14:27 +0900

    first push for study-javascript

---
+++ README.md
@@ -0,0 +1,19 @@
+# This project.
+
+for study javascript.
+
+# step
+
+1. install gulp-cli
+
+```
+npm install -g gulp-cli
+```
+
+2. install npm modules
+
+```
+npm install
+```
+
+3. enjoy
---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+let gulp = require('gulp');
+let browserSync = require('browser-sync').create();
+
+let cfg = {
+  path: './app/',
+  dist: './dist/'
+}
+
+gulp.task('build', function () {
+  return gulp
+    .src(cfg.path+'*')
+    .pipe(gulp.dest(cfg.dist)); 
+});
+
+gulp.task('serve', function () {
+  // 서버 실행
+  browserSync.init({
+    server: {
+      baseDir: cfg.path
+    }
+  });
+  // 감시
+  gulp.watch(cfg.path + '*', ['watch']);
+});
+
+gulp.task('watch', ['build'], function () {
+  browserSync.reload();
+  console.log('finish reload');
+});
+
+gulp.task('default', function () {
+  console.log('default run.');
+});
